142f0ca16a8b72aee594daa653737e3ae6bbf6ee
Liban <liban1808@gmail.com>
2021-07-03 23:03:52 +0100

    Amended App component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import "./App.css";
 import Header from "./components/Header";
 import Navigation from "./components/Navigation";
+import styled from "styled-components";
 function App() {
   return (
     <div className="App">
       <Header />
       <Navigation />
+      <Wrapper>
+        <Content role="main"></Content>
+      </Wrapper>
     </div>
   );
 }
 
 export default App;
+
+const Wrapper = styled.div`
+  min-height: 420px;
+  background: #fff;
+  width: 1240px;
+  margin: 0 auto;
+`;
+
+const Content = styled.div`
+  padding-right: 0px;
+  padding-left: 0px;
+  position: relative;
+  clear: both;
+`;
